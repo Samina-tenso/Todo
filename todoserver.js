781ddf5cc4822eb54370d2d5f7863b4156484686
Samina-tenso <tensammi@gmail.com>
2022-05-20 17:34:12 +0200

    delete working

--- todoserver.js
+++ todoserver.js
@@ -1,53 +1,69 @@
 const http = require("http")
 const fs = require("fs")
 const port = 4000
-
+const { URL } = require("url")
 const app = http.createServer((req, res) => {
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET, PATCH, DELETE, OPTIONS, POST, PUT");
     const items = req.url.split("/")
     console.log(items)
 
-    if (items[1] == "todos") {
-        if (req.method === "GET" && items.length === 2) {
-            res.statusCode = 200
-            res.setHeader("Content-Type", "application/json")
+    if (req.method === "GET" && items.length === 2) {
+        res.statusCode = 200
+        res.setHeader("Content-Type", "application/json")
+        fs.readFile("file.json", "utf8", (err, data) => {
+            if (err) {
+                console.error(err)
+            }
+            res.end(data.toString())
+        })
+
+    }
+    if (req.method === "POST" && items.length === 2) {
+
+        req.on("data", (chunk) => {
+            const newData = chunk.toString()
+            const parsednewData = JSON.parse(newData)
+
+
             fs.readFile("file.json", "utf8", (err, data) => {
                 if (err) {
-                    console.error(err)
+                    console.log(err)
+                    return
                 }
-                res.end(data.toString())
-            })
-
-        }
-        if (req.method === "POST") {
-
-            req.on("data", (chunk) => {
-                const newData = chunk.toString()
-                const parsednewData = JSON.parse(newData)
-
-
-                fs.readFile("file.json", "utf8", (err, data) => {
-                    if (err) {
-                        console.log(err)
-                        return
-                    }
-                    const parsedData = JSON.parse(data)
-                    parsedData.todos.push(parsednewData)
-                    const newTodos = JSON.stringify(parsedData)
-                    fs.writeFile("file.json", newTodos, (err) => {
-                        if (err) console.log(err)
-                    })
+                const parsedData = JSON.parse(data)
+                parsedData.todos.push(parsednewData)
+                const newTodos = JSON.stringify(parsedData)
+                fs.writeFile("file.json", newTodos, (err) => {
+                    if (err) console.log(err)
                 })
-
             })
-            res.end()
 
-        }
-    } else {
-        res.statusCode = 404
+        })
         res.end()
+
     }
+    if (req.method === "DELETE") {
+        let baseURL = "http://" + req.headers.host + "/"
+        let parsedUrl = new URL(req.url, baseURL)
+        let itemID = parsedUrl.searchParams.get("id");
+        console.log(itemID)
+        fs.readFile("file.json", "utf8", (err, data) => {
+            if (err) {
+                console.error(err)
+            }
+            let fileData = JSON.parse(data)
+            console.log(fileData.todos)
+
+
+
+            let newdata = fileData.todos.filter(function (itemz) {
+                return itemz.id != itemID
+            })
+            console.log(newdata)
+        })
+    } res.end()
+
 }
 )
 
