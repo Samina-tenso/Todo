576b9534a8a5da32e825fc70e02e6ac30b74ce36
Samina-tenso <tensammi@gmail.com>
2022-05-23 16:39:44 +0200

    works to console.log requested person

--- todoserver.js
+++ todoserver.js
@@ -19,7 +19,30 @@ const app = http.createServer((req, res) => {
         })
 
     }
-    if (req.method === "POST" && items.length === 2) {
+    if (req.method === "GET" && items[1].includes("id")) {
+        let baseURL = "http://" + req.headers.host + "/"
+        let parsedUrl = new URL(req.url, baseURL)
+        let itemID = parsedUrl.searchParams.get("id");
+        console.log(itemID)
+        fs.readFile("file.json", "utf8", (err, data) => {
+            if (err) {
+                return
+            }
+            let todoData = JSON.parse(data)
+            console.log(todoData)
+
+
+            let requestedPerson = todoData.find(function (person) {
+                return person.id = itemID
+            })
+            console.log(requestedPerson)
+
+        })
+
+
+    } res.end()
+
+    if (req.method === "POST") {
 
         req.on("data", (chunk) => {
             const newData = chunk.toString()
@@ -39,33 +62,38 @@ const app = http.createServer((req, res) => {
                 })
             })
 
+        })
+        res.end()
 
-        }
-        if (req.method === "POST") {
+    }
+    if (req.method === "DELETE") {
+        let baseURL = "http://" + req.headers.host + "/"
+        let parsedUrl = new URL(req.url, baseURL)
+        let itemID = parsedUrl.searchParams.get("id");
+        console.log(itemID)
+        fs.readFile("file.json", "utf8", (err, data) => {
+            if (err) {
+                console.error(err)
+            }
+            let fileData = JSON.parse(data)
+            console.log(fileData)
 
-            req.on("data", (chunk) => {
-                const newData = chunk.toString()
-                const parsednewData = JSON.parse(newData)
 
-
-                fs.readFile("file.json", "utf8", (err, data) => {
-                    if (err) {
-                        console.log(err)
-                        return
-                    }
-                    const parsedData = JSON.parse(data)
-                    parsedData.todos.push(parsednewData)
-                    const newTodos = JSON.stringify(parsedData)
-                    fs.writeFile("file.json", newTodos, (err) => {
-                        if (err) console.log(err)
-                    })
-                })
+            let newdata = fileData.filter(function (itemz) {
+                return itemz.id != itemID
 
             })
-            res.end()
+            console.log(newdata)
 
-        }
-    } else {
+            const JsonNewData = JSON.stringify(newdata)
+            fs.writeFile("file.json", JsonNewData, (err) => {
+                if (err) console.log(err)
+            })
+        })
+        res.end()
+    }
+
+    else {
         res.statusCode = 404
         res.end()
 
