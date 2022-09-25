954a821d8e2b92f2b60c35e3f69ac461d7db1cb4
Samina-tenso <tensammi@gmail.com>
2022-09-25 16:26:35 +0200

    removed spaces

--- todoBackend/todoserver.js
+++ todoBackend/todoserver.js
@@ -22,17 +22,10 @@ const app = http.createServer((req, res) => {
         res.end()
         return
     }
-
-
-
-
     const items = req.url.split("/")
     console.log(items)
-
     if (req.method === "GET" && !items[1].includes("id")) {
-
         res.writeHead(200, { "Content-Type": "application/json" });
-
         fs.readFile("file.json", "utf8", (err, data) => {
             if (err) {
                 console.error(err)
@@ -40,19 +33,9 @@ const app = http.createServer((req, res) => {
             res.write(data)
             res.end()
         })
-
         return
-
-
-
-
-
-
-
-
     } else if (req.method === "GET" && items[1].includes("id")) {
         res.writeHead(200, { "Content-Type": "application/json" });
-
         let baseURL = "http://" + req.headers.host + "/"
         let parsedUrl = new URL(req.url, baseURL)
         let itemID = parsedUrl.searchParams.get("id");
@@ -63,22 +46,15 @@ const app = http.createServer((req, res) => {
             }
             let todoData = JSON.parse(data)
             console.log(todoData)
-
-
             let requestedPerson = todoData.find(function (person) {
                 return person.id = itemID
             })
             console.log(requestedPerson)
-
             res.write(JSON.stringify(requestedPerson))
             res.end()
-
-
         })
         return
-
     } else if (req.method === "POST") {
-
         req.on("data", (chunk) => {
             const newData = chunk.toString()
             const parsednewData = JSON.parse(newData)
@@ -99,10 +75,8 @@ const app = http.createServer((req, res) => {
                     if (err) console.log(err)
                 })
             })
-
         })
         res.end()
-
     } else if (req.method === "DELETE") {
         let baseURL = "http://" + req.headers.host + "/"
         let parsedUrl = new URL(req.url, baseURL)
@@ -114,14 +88,10 @@ const app = http.createServer((req, res) => {
             }
             let fileData = JSON.parse(data)
             console.log(fileData)
-
-
             let newdata = fileData.filter(function (itemz) {
                 return itemz.id != itemID
-
             })
             console.log(newdata)
-
             const JsonNewData = JSON.stringify(newdata)
             fs.writeFile("file.json", JsonNewData, (err) => {
                 if (err) console.log(err)
@@ -130,108 +100,70 @@ const app = http.createServer((req, res) => {
         res.statusCode = 204
         res.end()
     } else if (req.method === "PATCH") {
-
         let baseURL = "http://" + req.headers.host + "/"
         let parsedUrl = new URL(req.url, baseURL)
         let itemID = parsedUrl.searchParams.get("id");
         console.log(itemID)
-
         req.on("data", (chunk) => {
             const newdata = JSON.parse(chunk)
             console.log(newdata)
-
-
-
             fs.readFile("file.json", "utf8", (err, data) => {
                 if (err) {
                     console.error(err)
                 }
                 let fileData = JSON.parse(data)
                 console.log(fileData)
-
-
                 let todoIndex = fileData.findIndex(function (todoItem) {
                     return todoItem.id === itemID
-
                 })
                 console.log(todoIndex)
-
-
-
                 let todoItem = fileData[todoIndex]
                 console.log(todoItem)
-
                 if (newdata.text) {
                     todoItem.text = newdata.text
                 }
                 fileData[todoIndex] = todoItem
                 console.log(fileData)
                 const JsonfileData = JSON.stringify(fileData)
-
-
                 fs.writeFile("file.json", JsonfileData, (err) => {
                     if (err) console.log(err)
-
-
                 })
                 res.write(JsonfileData)
                 res.end()
-
             })
-
         })
         return
-
-
     }
     if (req.method === "PUT") {
-
-
         let baseURL = "http://" + req.headers.host + "/"
         let parsedUrl = new URL(req.url, baseURL)
         let itemID = parsedUrl.searchParams.get("id");
         console.log(itemID)
-
         const randomID = crypto.randomUUID({ disableEntrophyCache: true })
         console.log(randomID)
-
         req.on("data", (chunk) => {
             let newdata = JSON.parse(chunk)
             console.log(newdata)
-
             fs.readFile("file.json", "utf8", (err, data) => {
                 if (err) {
                     console.error(err)
                 }
                 const fileData = JSON.parse(data)
                 console.log(fileData)
-
-
                 let todoIndex = fileData.findIndex(function (todoItem) {
                     return todoItem.id = itemID
-
                 })
                 console.log(todoIndex)
-
                 newdata = { ...newdata, "id": randomID }
                 console.log(newdata)
-
 
                 fileData[todoIndex] = newdata
 
                 console.log(fileData)
                 const JsonfileData = JSON.stringify(fileData)
-
-
-
                 fs.writeFile("file.json", JsonfileData, (err) => {
                     if (err) { console.log(err) }
-
-
                 })
-
-
-
             })
         })
         res.end()
@@ -239,10 +171,7 @@ const app = http.createServer((req, res) => {
         res.statusCode = 404
         res.end()
     }
-
 })
-
-
 app.listen(port, () => {
     console.log(`körs på ${port}`)
 })
